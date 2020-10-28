9e48e85f288554710ef2d6377486386ac2f6ffbe
Quentin Claes <quentin@cuustomer.com>
2020-10-28 17:54:21 +0100

    test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
+  res.setHeader("Content-Type", "text/xml");
+  res.write(`<?xml version="1.0" ?>
+  <rss version="2.0">
+    <channel>
+        <title>test de flux rss par Quentin Claes</title>
+        <link>https://www.quentinclaes.be</link>
+        <description>test</description>
+        <language>en</language>
+        <lastBuildDate>test</lastBuildDate>
+    </channel>
+  </rss>`);
+  res.end();
 }
